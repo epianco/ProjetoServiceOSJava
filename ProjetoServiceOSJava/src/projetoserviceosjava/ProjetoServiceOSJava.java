2b04f0204a27cd914976d30c6a865ded5d3a05ea
epianco <vinicius.dsp19@gmail.com>
2020-11-14 18:39:18 -0200

    teste Comit

--- ProjetoServiceOSJava/src/projetoserviceosjava/ProjetoServiceOSJava.java
+++ ProjetoServiceOSJava/src/projetoserviceosjava/ProjetoServiceOSJava.java
@@ -15,7 +15,7 @@ public class ProjetoServiceOSJava {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here trar
+        System.out.println("Hello Get");
     }
     
 }
